367287ab328aa28f490343b197b6efec8ad7fa5e
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-06 17:03:07 +0200

    added condition for negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,8 +4,11 @@ number = random.randint(-10000, 10000)
 
 if number < 0:
     number = number * -1
+    num = (number % 10) * -1
+    number *= -1
 
-num = number % 10
+else:
+    num = number % 10
 
 if num < 6 and num != 0:
     print(f"Last digit of {number} is {num} and is less than 6 and not 0")
