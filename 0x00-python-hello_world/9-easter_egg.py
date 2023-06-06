64923ff2bfc26e75daf44b25274d9d15c0acf0b0
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-06 02:19:59 +0200

    removed long repeated words

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
 
+b = "better than"
+a = "Although"
+i = "implementation"
+n = "never"
+e = "explain"
 note = f"""The Zen of Python, by Tim Peters
 
-Beautiful is better than ugly.
-Explicit is better than implicit.
-Simple is better than complex.
-Complex is better than complicated.
-Flat is better than nested.
-Sparse is better than dense.
+Beautiful is {b} ugly.
+Explicit is {b} implicit.
+Simple is {b} complex.
+Complex is {b} complicated.
+Flat is {b} nested.
+Sparse is {b} dense.
 Readability counts.
 Special cases aren't special enough to break the rules.
-Although practicality beats purity.
-Errors should never pass silently.
+{a} practicality beats purity.
+Errors should {n} pass silently.
 Unless explicitly silenced.
 In the face of ambiguity, refuse the temptation to guess.
 There should be one-- and preferably only one --obvious way to do it.
-Although that way may not be obvious at first unless you're Dutch.
-Now is better than never.
-Although never is often better than *right* now.
-If the implementation is hard to explain, it's a bad idea.
-If the implementation is easy to explain, it may be a good idea.
+{a} that way may not be obvious at first unless you're Dutch.
+Now is {b} than {n}.
+{a} {n} is often {b} *right* now.
+If the {i} is hard to {e}, it's a bad idea.
+If the {i} is easy to {e}, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!"""
 print(note)
