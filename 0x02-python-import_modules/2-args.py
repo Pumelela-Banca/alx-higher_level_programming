3a5b601f9726420ab33d50c9355cee108a132547
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-08 07:07:30 -0700

    used ubuntu to add permissions

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,10 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        print(".")
+        print("0 arguments.")
     elif len(sys.argv) == 2:
         print(f"1 argument:\n1: {sys.argv[1]}")
     else:
         print(f"{len(sys.argv)} arguments:")
-        for m in range(1, len(sys.argv) + 1):
-            print(f"{m} argument:\n1: {sys.argv[m]}")
+        for m in range(1, len(sys.argv)):
+            print(f"{m}: {sys.argv[m]}")
