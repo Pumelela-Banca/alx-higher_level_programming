b1ff297aee3afcdf51f0c3d916a7c6d88e6ac286
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-08 07:27:59 -0700

    added test files and correct print statements

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     elif len(sys.argv) == 2:
         print(f"1 argument:\n1: {sys.argv[1]}")
     else:
-        print(f"{len(sys.argv)} arguments:")
+        print(f"{len(sys.argv) - 1} arguments:")
         for m in range(1, len(sys.argv)):
             print(f"{m}: {sys.argv[m]}")
