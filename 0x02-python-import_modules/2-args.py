f8b98ff73068c681956a7b28eaa59dc21c408213
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-08 15:27:47 +0200

    prints arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import sys
+
+if __main__ == "__name__":
+    if len(sys.argv) == 1:
+        print(".")
+    elif len(sys.argv) == 2:
+        print(f"1 argument:\n1: {sys.argv[1]}")
+    else:
+        print(f"{len(sys.argv)} arguments:")
+        for m in range(1, len(sys.argv) + 1):
+            print(f"{m} argument:\n1: {sys.argv[m]}")
