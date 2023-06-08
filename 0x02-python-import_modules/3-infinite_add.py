87342785ed28f849d686718e7d157c5bafa2f347
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-08 07:15:59 -0700

    Q-4 added new code for empty commands and single lines

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,11 @@ from sys import argv
 
 if __name__ == "__main__":
     sum_all = 0
-    for x in argv:
-        sum_all += int(x)
-    print(sum_all)
+    if len(argv) == 1:
+        print(0)
+    elif len(argv) == 2:
+        print(argv[1])
+    else:
+        for x in argv[1:]:
+            sum_all += int(x)
+        print(sum_all)
