4491884b16bf33d1e1b7661569c26ec083e73e25
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-10 02:52:16 +0200

    Readme and 0-print_list_integer.py that prints list items

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    """
+    print_list_integer - prints list
+    my_list: list to print
+    """
+    for x in my_list:
+        print("{}".format(x))
