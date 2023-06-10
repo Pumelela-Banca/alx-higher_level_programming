a66f5fc2032f2a724eb552722c7d45e3b50b2280
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-10 03:00:52 +0200

    return index

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    """
+    element_at - returns item at index idx
+    @my_list: list
+    @idx: index
+    """
+
+    if len(my_list) == 0:
+        return None
+    elif len(my_list) < (idx + 1):
+        return None
+    else:
+        return my_list[idx]
