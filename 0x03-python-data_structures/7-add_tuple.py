f0bbc7ed159689828c91f9664d6eda0c75d535bf
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-11 23:23:30 +0200

    adds two tuples and returns sum of indexes

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    """
+    add_tuple - adds index (1, 2) from two tuples
+    @tuple_a: tuple
+    @tuple_b: tuple
+    Return: tuple with two interges
+    """
+    if len(tuple_a) < 2:
+        tuple_a = (tuple_a[0], 0)
+    elif len(tuple_b) < 2:
+        tuple_b = (tuple_b[0], 0)
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
