7e68aa3b2cb37a67fb65818a4395e64631057007
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-11 23:29:46 +0200

    returns first letter of string and its lenght

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    """
+    multiple_returns - returns lenght of  string and first character
+    @sentence: string
+    """
+    if not sentence:
+        return (None,)
+    return (len(sentence), sentence[0])
