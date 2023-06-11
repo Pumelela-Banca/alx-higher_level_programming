cdfebc78ce072b774fe694af21573fd1f06fefcc
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-12 01:35:00 +0200

    added conditio for empty tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -12,5 +12,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len(tuple_b) < 2:
         tuple_b = (tuple_b[0], 0)
     elif len(tuple_a) < 2 and len(tuple_b) < 2:
-        return (,)
+        return (0, 0)
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
