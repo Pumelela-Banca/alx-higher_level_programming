eaf718d7c104ee91670fd66a901ab9cfc2bd5344
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-11 23:35:24 +0200

    finds max number in a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    """
+    max_integer - returns biggest integer in list
+    @my_list: list
+    """
+    if len(my_list) == 0:
+        return None
+    big = 0
+    for x in my_list:
+        if x > big:
+            continue
+        big = x
+    return big
