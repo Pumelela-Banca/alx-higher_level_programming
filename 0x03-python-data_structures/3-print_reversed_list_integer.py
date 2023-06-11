bae013e1274d344e39ffe44cc946eb488a0d23a4
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-12 00:58:23 +0200

    added empty list condition

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,10 +1,12 @@
-#!/usr/bin/python3
+
 def print_reversed_list_integer(my_list=[]):
     """
     print_reversed_list_integer - pprint list in reverse
     @my_list: list
     """
     msu = len(my_list)
+    if msu == 0:
+        print([])
     start = -1
     while True:
         print("{:d}".format(my_list[start]))
