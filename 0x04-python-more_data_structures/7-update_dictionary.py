8f1c4694c3538ba4e8f5492e213829717d55e2cc
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-15 01:06:03 +0200

    added a return for updated dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+
 
 def update_dictionary(a_dictionary, key, value):
     """
@@ -11,3 +11,4 @@ def update_dictionary(a_dictionary, key, value):
         a_dictionary[key] = value
     else:
         a_dictionary.update({key: value})
+    return a_dictionary
