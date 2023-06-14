10f13a358f6337d06d41e5421165cfe9f497037f
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-14 03:16:34 +0200

    added bese cases for empty list, idx = 0 and len = idx

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -7,7 +7,7 @@ def delete_at(my_list=[], idx=0):
     @idx: index to delete
     Return: new list
     """
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list) or len(my_list) == 0:
         return my_list
     del my_list[idx]
     return my_list
