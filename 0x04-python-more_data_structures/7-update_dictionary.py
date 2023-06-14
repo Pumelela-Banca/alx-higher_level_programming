cba65468feef0cc9337ba2d947b09a459c7597f9
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-14 23:52:49 +0200

    updates dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """
+    update a dictionary
+    @a_dictionary: data
+    @key: update
+    @value: new data
+    """
+    a_dictionary[key] = value
+
