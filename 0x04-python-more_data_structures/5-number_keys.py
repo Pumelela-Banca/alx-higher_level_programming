c356f8c2c3a2ef44b938cd2db37090696dd28870
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-14 22:30:45 +0200

    counts number of keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """
+    number_keys - counts keys
+    @a_dictionary: data
+    Return: sum of keys
+    """
+    return len(a_dictionary.keys())
