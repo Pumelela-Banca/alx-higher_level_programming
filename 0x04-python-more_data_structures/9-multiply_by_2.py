6235bd541abc94b0d27b608ccd557cf241a23876
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-15 01:32:25 +0200

    removed white space

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -6,4 +6,4 @@ def multiply_by_2(a_dictionary):
     @a_dictionary: data
     Return: new dictionary
     """
-    return {x : y * 2 for (x, y) in a_dictionary.items()}
+    return {x :y * 2 for (x, y) in a_dictionary.items()}
