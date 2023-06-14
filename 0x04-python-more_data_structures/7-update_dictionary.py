8c910770135bfe9aee33898b86b5d7d45331da07
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-15 01:07:32 +0200

    added a condition for None value or key

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -7,6 +7,8 @@ def update_dictionary(a_dictionary, key, value):
     @key: update
     @value: new data
     """
+    if key == None or value == None:
+        return a_dictionary
     if key in a_dictionary.keys():
         a_dictionary[key] = value
     else:
