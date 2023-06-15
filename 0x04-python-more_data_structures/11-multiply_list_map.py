7e491e62154d9e7c7f9b358254ee11e045b91537
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-15 04:30:31 +0200

    removed more lines

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,11 +1,2 @@
 #!/usr/bin/python3
-
-def multiply_list_map(my_list=[], number=0):
-    """
-    multiply_list_map - use map to multiply list
-    @my_list: list
-    @number: number to use
-    """
-    if len(my_list) == 0:
-        return []
-    return list(map(lambda x: x * number, my_list))
+def multiply_list_map(my_list=[], number=0): return list(map(lambda x: x * number, my_list))
