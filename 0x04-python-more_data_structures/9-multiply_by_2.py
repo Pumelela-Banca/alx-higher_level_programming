31e657deb0a449041daca68f1d0321c7bc6ba7d9
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-15 04:19:04 +0200

    added pycodestyle

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -8,4 +8,4 @@ def multiply_by_2(a_dictionary):
     """
     if a_dictionary is None:
         return None
-    return {x :y * 2 for (x, y) in a_dictionary.items()}
+    return {x : y * 2 for (x, y) in a_dictionary.items()}
