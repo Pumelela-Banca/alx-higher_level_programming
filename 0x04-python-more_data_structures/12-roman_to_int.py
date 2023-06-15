21fdaefe10cc4611f9fbd221a31ad0a2f6073816
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-15 03:53:32 +0200

    converts rmen numerals to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    """
+    roman_to_int - turns roman numbers to int
+    @roman_string: roman string
+    Return: integer
+    """
+    if not isinstance(roman_string, str) or len(roman_string) == 0 or roman_string == None:
+        return 0
+    names = {"I": 1, "II": 2, "III": 3, "IV": 4, "X": 10, "L": 50, "C": 100,
+            "V": 5, "VI": 6, "VII": 7, "VIII":8, "IX": 9, "D": 500, "M": 1000}
+    sum_all = 0
+    if roman_string in names.keys():
+        return names[roman_string]
+    for x in roman_string:
+        sum_all += names[x]
+    return sum_all
