f4c4b3a8ef1b6a3fd5551966ac8ab8e15e78eb67
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-16 02:10:14 +0200

    freed memory from new

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -36,7 +36,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		go = go->next;
 		at++;
 	}
-	
+	free(new);
 	if (at == (idx + 1))
 		return (add_dnodeint_end(h, n));
 	return (NULL);
