714a069ad0a132687fd2cfe9a82f3390f2fb6b32
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-27 01:43:46 +0200

    added range with slicing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,7 +10,7 @@ def safe_print_list(my_list=[], x=0):
     """
     num = 0
     try:
-        for n in my_list:
+        for n in my_list[:x]:
             print(n, end="")
             num += 1
     except IndexError:
