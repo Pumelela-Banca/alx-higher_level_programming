7dd74a81978348c54d88b434eef62765b4c465e2
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-27 02:41:02 +0200

    divides two lists

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    """
+    divides list index by each
+    @my_list_1: list 1
+    @my_list_2: list 2
+    @list_length: amount to divisions
+    Return: new list
+    """
+    new = [0] * list_length
+    no = 0
+    for x in range(0, list_length):
+        try:
+            no = my_list_1[x] / my_list_2[x]
+        except TypeError:
+            new[x] = 0
+            print("wrong type")
+        except ZeroDivisionError:
+            new[x] = 0
+            print("division by 0")
+        except IndexError:
+            new[x] = 0
+            print("out of range")
+        finally:
+            new[x] = no
+    return new
