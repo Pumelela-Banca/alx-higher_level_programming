f4f93aa187b953f41df2f7e3b845a90a7f57f4c0
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-27 22:50:22 +0200

    class adds method to calculate area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    defines a square
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = size
+
+    def area(self):
+        """
+        current area
+        Return: area
+        """
+        return self._Square__size * self._Square__size
