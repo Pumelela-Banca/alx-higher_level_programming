279ad2c2192efd72f003c125881e11ae59c193e9
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-28 20:32:01 +0200

    empty files for #advanced tasks

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
 
 """
-This is a class
+Defines a square class that can be printed using my_print
 """
-
 
 class Square:
     """
     defines a square
     """
 
-    def __init__(self, size=0):
-        if not isinstance(size, int):
+    def __init__(self, x=0, position=(0, 0)):
+        if not isinstance(x, int):
             raise TypeError('size must be an integer')
-        if size < 0:
+        if x < 0:
             raise ValueError("size must be >= 0")
-        self._Square__size = size
-        self.size = self._Square__size
+        self._Square__size = x
+        self.size = x
 
     def area(self):
         """
         current area
         Return: area
         """
-        return self._Square__size * self._Square__size
+        return self.size * self.size
 
     def size(self, value=None):
         """
@@ -31,8 +30,36 @@ class Square:
         """
         if value is None:
             return self.size
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError('size must be an integer')
         if value < 0:
             raise ValueError("size must be >= 0")
         self.size = value
+
+    def __setattr__(self, key, value):
+        """
+        checks value of size
+        :key: instance
+        :value: value
+        """
+        if key != '_Square__size' and key == "size":
+            if not isinstance(value, int):
+                raise TypeError('size must be an integer')
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            self.__dict__[key] = value
+        else:
+            self.__dict__[key] = value
+
+    def my_print(self):
+        """
+        prints a square of #
+        """
+        if self.size == 0:
+            print()
+        elif self.size == 1:
+            print("#")
+        else:
+            s = "#" * self.size
+            for _ in range(self.size):
+                print(s)
