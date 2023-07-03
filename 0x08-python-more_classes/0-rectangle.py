5bf9706f983e45f781c212bec93c571081056a21
Pumelela-Banca <pumelelabanca@gmail.com>
2023-07-03 12:10:20 +0200

    empty class rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Class that defines a rectangle and its methords will be added
+gradually
+
+"""
+
+class Rectangle:
+    """
+    Empty class defining Rectangle
+    """
+    pass
