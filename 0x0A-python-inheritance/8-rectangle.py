34682498529d5e4436890ab2734f49ae76425af2
Pumelela-Banca <pumelelabanca@gmail.com>
2023-07-11 01:31:15 +0200

    adds init to besegeometry

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+module with class that inherits from BaseGeometry
+"""
+
+from 7-base_geometry import BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """
+    inherits from BaseGeometry methods and adds __init__
+    """
+    def __init__(self, width, height):
+        __height = height
+        __width = width
+        self.integer_validator("'Rectangle'", __width)
+        self.integer_validator("'Rectangle'", __height)
