3aaf5f334e53967527bb159da7dc302f18e8b3d2
Pumelela-Banca <pumelelabanca@gmail.com>
2023-07-11 12:06:27 +0200

    added new docummentation

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 """
-module with class that inherits from BaseGeometry
+a module with class that inherits from BaseGeometry
 and adds init that adds width and height
 """
 
 
 class Rectangle(BaseGeometry):
     """
-    class inherits from BaseGeometry methods and adds __init__ with
+    a class inherits from BaseGeometry methods and adds __init__ with
     width and height
     """
 
