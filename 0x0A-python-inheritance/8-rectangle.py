1898aad852dfff1342b71e4cc542590f27c031ba
Pumelela-Banca <pumelelabanca@gmail.com>
2023-07-11 20:44:46 +0200

    added getgeometry class to file

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,9 +1,30 @@
 #!/usr/bin/python3
 
 """
-a module with class that inherits from BaseGeometry
-and adds init that adds width and height
+a module with class that inherits from BaseGeometry and adds
+init that adds width and height to base geometry
 """
+
+
+class BaseGeometry:
+    """
+    has method that raises exception
+    """
+
+    def area(self):
+        """
+        not implemented yet
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Looks to validate integers
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value < 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
