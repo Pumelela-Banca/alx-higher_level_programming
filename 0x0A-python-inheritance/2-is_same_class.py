6939eb498e4a0112c1d03685e67e88d45d51e095
Pumelela-Banca <pumelelabanca@gmail.com>
2023-07-11 11:47:49 +0200

    changed if statement to type(obj) == a_class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -11,8 +11,7 @@ def is_same_class(obj, a_class):
     exactly an instance of the specified class ;
     otherwise False
     """
-    if isinstance(obj, a_class) and \
-            a_class.__name__ != object.__name__:
+    if type(obj) == a_class:
         return True
     else:
         return False
