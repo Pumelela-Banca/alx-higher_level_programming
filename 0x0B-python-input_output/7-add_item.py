0c3f1c843ae0b5a5a30e7dbc3d6fa12787912ce4
Pumelela-Banca <pumelelabanca@gmail.com>
2023-07-12 03:02:14 +0200

    use readline to see if list empty

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,13 +8,13 @@ import json
 from sys import argv
 
 
-save_to_json_file = __import__('5-save_to_json_file.py').save_o_json_file
-load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 if len(argv) == 1:
     with open("add_item.json", "w+") as f:
-        data = json.load(f)
+        data = f.readlines()
         if not data:
             json.dump([], f)
 else:
