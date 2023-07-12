1dbfa94e50608c4e13056978781055d953f2d6b0
Pumelela-Banca <pumelelabanca@gmail.com>
2023-07-12 02:57:36 +0200

    save changes

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -9,4 +9,9 @@ def from_json_string(my_str):
     """
     creates python object from JSON string
     """
+    new = ''
+    for x in my_str:
+        if x == '\n':
+            continue
+        new += x
     return eval(my_str)
