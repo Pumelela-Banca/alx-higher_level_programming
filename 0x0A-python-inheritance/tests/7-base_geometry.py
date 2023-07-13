42512b18d0a2b2e64ce2498324c9c6da67d73a99
Pumelela-Banca <pumelelabanca@gmail.com>
2023-07-13 12:12:51 +0200

    changed error message to match test 'age'

--- 0x0A-python-inheritance/tests/7-base_geometry.py
+++ 0x0A-python-inheritance/tests/7-base_geometry.py
@@ -24,6 +24,3 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-
-
-print(BaseGeometry().integer_validator("age"))
