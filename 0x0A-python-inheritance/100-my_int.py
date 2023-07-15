4f52cd6177b7cfed49fcb5fd3fcabeaa8e5e248b
Pumelela-Banca <pumelelabanca@gmail.com>
2023-07-15 19:21:53 +0200

    inherits from int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+a module with class MyInt that inherits from int
+"""
+
+
+class MyInt(int):
+    """
+    inherits from int but switches == and !=
+    """
+
+    def __eq__(self, other):
+        int.__ne__(self, other)
+
+    def __ne__(self, other):
+        int.__eq__(self, other)
