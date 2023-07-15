886edcf93b79f8bf1354056a1961a48bf3afcf4b
Pumelela-Banca <pumelelabanca@gmail.com>
2023-07-15 19:26:27 +0200

    returns opposite

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -11,7 +11,7 @@ class MyInt(int):
     """
 
     def __eq__(self, other):
-        int.__ne__(self, other)
+        return int.__ne__(self, other)
 
     def __ne__(self, other):
-        int.__eq__(self, other)
+        return int.__eq__(self, other)
