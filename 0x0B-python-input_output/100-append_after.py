c5052b7b5a60b157aed106a24de16fb0443efa7f
Pumelela-Banca <pumelelabanca@gmail.com>
2023-07-15 18:42:33 +0200

    adds lines after word is found

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -11,4 +11,14 @@ def append_after(filename="", search_string="", new_string=""):
     unction that inserts a line of text to a file, after each
     line containing a specific string
     """
+    new = []
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        for ln in lines:
+            new.append(ln)
+            if search_string in ln:
+                new.append([new_string])
 
+    with open(filename, 'w') as f:
+        for x in new:
+            f.writelines(x[0])
