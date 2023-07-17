e3c5c61dc97255c142341ae2030dfea6d32fa0e6
Pumelela-Banca <pumelelabanca@gmail.com>
2023-07-17 23:31:36 +0200

    added getter and setter for size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,32 +12,31 @@ class Square(Rectangle):
     a square class derived from Rectangle with __str__ method changed
     """
     def __init__(self, size, x=0, y=0, id=None):
-        Rectangle.__init__(self, size, size, x=x, y=y, id=id)
-        self.size = size
+        Rectangle.__init__(self,size, size, x=x, y=y, id=id)
         self.__x = x
         self.__y = y
 
     def __str__(self):
         return f"[Square] ({self.id}) {self.__x}/{self.__y} - {self.size}"
 
+    @property
+    def size(self):
+        return self.height
+
+    @size.setter
+    def size(self, value):
+        self.validate(value, "size")
+        self.__height = value
 
 if __name__ == "__main__":
 
     s1 = Square(5)
     print(s1)
-    print(s1.area())
-    s1.display()
+    print(s1.size)
+    s1.size = 10
+    print(s1)
 
-    print("---")
-
-    s2 = Square(2, 2)
-    print(s2)
-    print(s2.area())
-    s2.display()
-
-    print("---")
-
-    s3 = Square(3, 1, 3)
-    print(s3)
-    print(s3.area())
-    s3.display()
+    try:
+        s1.size = "9"
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
