470685bce49475998fc242f9b3a572962e28a69c
Pumelela-Banca <pumelelabanca@gmail.com>
2023-07-17 23:47:05 +0200

    added new __size private variable

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -15,18 +15,44 @@ class Square(Rectangle):
         Rectangle.__init__(self,size, size, x=x, y=y, id=id)
         self.__x = x
         self.__y = y
+        self.__size = size
 
     def __str__(self):
-        return f"[Square] ({self.id}) {self.__x}/{self.__y} - {self.size}"
+        return f"[Square] ({self.id}) {self.__x}/{self.__y}" + \
+            f"- {self.__size}"
+
+    def update(self, *args, **kwargs):
+        if args and args is not None:
+            try:
+                self.id = args[0]
+                self.__size = args[1]
+                self.__y = args[2]
+                self.__x = args[3]
+            except IndexError:
+                return
+        else:
+            for x in kwargs:
+                self.alt_set(x, kwargs[x])
+
+    def alt_set(self, key, value):
+        if key == "size":
+            self.validate(value, "width")
+            self.__size = value
+        elif key == "y":
+            self.validate(value, "y")
+            self.__y = value
+        elif key == "x":
+            self.validate(value, "x")
+            self.__x = value
 
     @property
     def size(self):
-        return self.height
+        return self.__size
 
     @size.setter
     def size(self, value):
         self.validate(value, "size")
-        self.__height = value
+        self.__size = value
 
 if __name__ == "__main__":
 
@@ -40,3 +66,27 @@ if __name__ == "__main__":
         s1.size = "9"
     except Exception as e:
         print("[{}] {}".format(e.__class__.__name__, e))
+
+    s1 = Square(5)
+    print(s1)
+
+    s1.update(10)
+    print(s1)
+
+    s1.update(1, 2)
+    print(s1)
+
+    s1.update(1, 2, 3)
+    print(s1)
+
+    s1.update(1, 2, 3, 4)
+    print(s1)
+
+    s1.update(x=12)
+    print(s1)
+
+    s1.update(size=7, y=1)
+    print(s1)
+
+    s1.update(size=7, id=89, y=1)
+    print(s1)
