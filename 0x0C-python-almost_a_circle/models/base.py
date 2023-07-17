145d5846701677ecc317e09216219d051d21f19c
Pumelela-Banca <pumelelabanca@gmail.com>
2023-07-18 00:05:10 +0200

    added to_json_string method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,6 +5,8 @@ module contains a base class for all classes with
 attribute id
 """
 
+import json
+
 
 class Base:
     """
@@ -20,20 +22,9 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-
-if __name__ == "__main__":
-
-    b1 = Base()
-    print(b1.id)
-
-    b2 = Base()
-    print(b2.id)
-
-    b3 = Base()
-    print(b3.id)
-
-    b4 = Base(12)
-    print(b4.id)
-
-    b5 = Base()
-    print(b5.id)
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
