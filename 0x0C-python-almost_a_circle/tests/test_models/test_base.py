dde4476e27d1d5e881714612b1aa5ddf1da2def4
Pumelela-Banca <pumelelabanca@gmail.com>
2023-07-18 03:30:57 +0200

    added base test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -12,3 +12,12 @@ from models import Base
 
 class TestSquare(unittest.TestCase):
     """test id track"""
+    def test_ids(self):
+        r1 = Base(10, 2)
+        self.assertEqual(r1.id, 1)
+
+        r2 = Base(2, 10)
+        self.assertEqual(r2.id, 2)
+
+        r3 = Base(10, 2, 0, 0, 12)
+        self.assertEqual(r3.id, 12)
