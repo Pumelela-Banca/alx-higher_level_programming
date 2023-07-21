a0d94da72f75d8d6abe67587ef97eea0775e8a27
Pumelela-Banca <pumelelabanca@gmail.com>
2023-07-22 00:32:30 +0200

    added added  to square

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -39,6 +39,13 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(box.height, 2)
         self.assertEqual(box.x, 3)
         self.assertEqual(box.y, 4)
+        # test id
+        box = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(box.width, 1)
+        self.assertEqual(box.height, 2)
+        self.assertEqual(box.x, 3)
+        self.assertEqual(box.y, 4)
+        self.assertEqual(box.id, 5)
 
     """test errors"""
     def test_errors(self):
@@ -55,7 +62,35 @@ class TestSquare(unittest.TestCase):
             Rectangle(1, 2, 3, "4")
         self.assertEqual(str(e.exception), "y must be an integer")
 
+    """test negative"""
+    def test_negatives(self):
+        with self.assertRaises(ValueError) as e:
+            Rectangle(-1, 2)
+        self.assertEqual(str(e.exception), "width must be > 0")
+        with self.assertRaises(ValueError) as e:
+            Rectangle(1, -2)
+        self.assertEqual(str(e.exception), "height must be > 0")
+        with self.assertRaises(ValueError) as e:
+            Rectangle(1, 2, -3)
+        self.assertEqual(str(e.exception), "x must be >= 0")
+        with self.assertRaises(ValueError) as e:
+            Rectangle(1, 2, 3, -4)
+        self.assertEqual(str(e.exception), "y must be >= 0")
 
+    """test zero x and y"""
+    def test_zero(self):
+        with self.assertRaises(ValueError) as e:
+            Rectangle(0, 2)
+        self.assertEqual(str(e.exception), "width must be > 0")
+        with self.assertRaises(ValueError) as e:
+            Rectangle(1, 0)
+        self.assertEqual(str(e.exception), "height must be > 0")
+
+    """test area"""
+    def test_area(self):
+        self.assertEqual(Rectangle(2, 2).area(), 4)
+
+        self.assertEqual(Rectangle(2, 2).area(), 4)
 
 
 if __name__ == '__main__':
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -15,8 +15,20 @@ class TestSquare(unittest.TestCase):
     def test_init(self):
         box = Square(4, 6, 3)
         self.assertEqual(box.size, 4)
-        #self.assertEqual(box.id, 7)
+        self.assertEqual(box.id, 20)
         self.assertEqual(box.x, 6)
+
+    """test area"""
+    def test_area(self):
+        self.assertEqual(Square(2, 2).area(), 4)
+
+    """test zero x and y"""
+
+    def test_zero(self):
+        with self.assertRaises(ValueError) as e:
+            Square(0, 2)
+        self.assertEqual(str(e.exception), "height must be > 0")
+
 
 
 if __name__ == '__main__':
