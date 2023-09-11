0b55bf75394ef49d484dd7ff5de6e3b2a4fe089d
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-12 01:47:55 +0200

    searches for second biggets value

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+const { argv } = require('process');
+
+if (argv.length < 4) {
+  console.log(0);
+} else {
+  let big = Number.NEGATIVE_INFINITY;
+  let second = big;
+  for (let i = 2; i < argv.length; i++) {
+    if (parseInt(argv[i]) > big) {
+      second = big;
+      big = parseInt(argv[i]);
+    } else if (parseInt(argv[i]) > second) {
+      second = parseInt(argv[i]);
+    }    
+  }
+  console.log(second);
+}
