194ce810ef86a9d43d4b151d27780019eab7c195
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-12 02:16:39 +0200

    concats arguments

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const { argv } = require('process');
+
+if (argv[2] === undefined && argv[3] === undefined) {
+  console.log('undefined is undefined');
+} else if (argv[3] === undefined) {
+  console.log(argv[2] + ' is undefined');
+} else {
+    console.log(argv[2] + ' is ' + argv[3]); 
+}
