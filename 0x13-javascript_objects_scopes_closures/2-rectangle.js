07a0b7bf22b81553c60eefb5250961c96746a267
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-13 00:53:15 +0200

    added do nothing comment

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,9 +2,9 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (typeof w !== "number" || typeof w !== "number" ||
+    if (typeof w !== 'number' || typeof w !== 'number' ||
       w < 1 || h < 1) {
-      return;   
+      // do nothing
     } else {
       this.width = w;
       this.height = h;
