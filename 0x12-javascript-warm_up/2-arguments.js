c50f39aad36c5be50bbe7cfdee2727d088222302
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-12 02:07:39 +0200

    added else clause

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,5 +4,7 @@ const { argv } = require('process');
 if (argv.length < 3) {
   console.log('No argument');
 } else if (argv.length >= 3) {
-    console.log('Argument found');
+  console.log('Argument found');
+} else {
+    console.log('Arguments found');
 }
