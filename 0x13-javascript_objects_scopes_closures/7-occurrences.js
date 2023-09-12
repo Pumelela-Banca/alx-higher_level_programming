6a25d5e47eda0652b2ddc15077608124df7a1d0d
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-13 01:42:29 +0200

    count occurances

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,4 +1,10 @@
 
 exports.nbOccurences = function (list, searchElement) {
-  
-}
+  let c = 0;
+  for (let i = 0; i < list.lenght; i++) {
+    if (list[i] === searchElement) {
+      c++;
+    }
+  }
+  return c;
+};
