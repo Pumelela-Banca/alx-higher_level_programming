39f3e9fc9662f8790b2662984b0374874d4fc938
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-13 04:15:12 +0200

    separated prints

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
-console.log(list);
-console.log(list.map((value, index) => value * index));
+const dict = require('./101-data').dict;
+const newDict = {};
