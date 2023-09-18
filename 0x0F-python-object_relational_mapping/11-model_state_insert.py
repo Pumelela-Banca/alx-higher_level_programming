766ad77015aa8e3a103ee1ca5accae0dff0c930c
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-19 01:38:39 +0200

    adds state to states

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+adds Louisiana to the db
+"""
+
+import sys
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        """mysql+mysqldb://{}:{}@localhost:3306/{}""".
+        format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).count()
+    luisiana = State()
+    luisiana.name = "Louisiana"
+    luisiana.id = states + 1
+    session.add(luisiana)
+    session.commit()
+    session.close()
+    print(luisiana.id)
