66d83ad026dbb26be59528aea97d972977538c23
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-18 10:18:57 +0200

    prints states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+script that lists all states from the database
+hbtn_0e_0_usa
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306,
+                         password=sys.argv[2], database=sys.argv[3])
+    with db.cursor() as cur:
+        cur.excecute("SELECT * FROM states ORDER BY id")
+        for i in cur.fetchall():
+            print(i)
+        cur.close
+    db.close
