43ee7ebbe90a001e8e0c37b99a058c1131c73773
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-18 13:17:38 +0200

    user selection

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+script that lists all states from the database
+hbtn_0e_0_usa filter using user input
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306,
+                         passwd=sys.argv[2], db=sys.argv[3])
+    with db.cursor() as cur:
+        cur.execute("""SELECT * FROM states
+         WHERE name={} ORDER BY id""".format(sys.argv[4]))
+        for i in cur.fetchall():
+            print(i)
+    db.close
