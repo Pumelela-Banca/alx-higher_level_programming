5135a3266dcb275eaba64435df34931ea88b4c7f
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-18 10:22:42 +0200

    db = database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,7 @@ import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306,
-                         password=sys.argv[2], database=sys.argv[3])
+                         password=sys.argv[2], db=sys.argv[3])
     with db.cursor() as cur:
         cur.excecute("SELECT * FROM states ORDER BY id")
         for i in cur.fetchall():
