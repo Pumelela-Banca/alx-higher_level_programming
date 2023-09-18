e512a11c80d9e29abde0c77ba579d40df1136032
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-18 11:35:07 +0200

    changed argv locations

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,8 +10,8 @@ import MySQLdb
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306,
-                         passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", user=sys.argv[2], port=3306,
+                         passwd=sys.argv[3], db=sys.argv[4])
     with db.cursor() as cur:
         cur.execute("SELECT * FROM states ORDER BY id")
         for i in cur.fetchall():
