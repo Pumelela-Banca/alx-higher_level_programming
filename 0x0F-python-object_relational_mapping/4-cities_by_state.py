a25b52a1f8ac66194192a2e4a80d3095e078d07f
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-18 15:23:23 +0200

    shows output needed

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3])
 
     with db.cursor() as cur:
-        cur.execute("""SELECT * FROM cities
+        cur.execute("""SELECT cities.id, cities.name,
+         states.name FROM cities
          JOIN states ON cities.state_id = states.id
          ORDER BY cities.id""")
         for i in cur.fetchall():
