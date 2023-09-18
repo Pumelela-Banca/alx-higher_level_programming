55f1dafe9a14fe23c9f3c3166e4590f22c383da3
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-18 15:20:27 +0200

    filters two tables

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+lists all cities of a state
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306,
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    with db.cursor() as cur:
+        cur.execute("""SELECT * FROM cities
+         JOIN states ON cities.state_id = states.id
+         ORDER BY cities.id""")
+        for i in cur.fetchall():
+            print(i)
+    db.close()
