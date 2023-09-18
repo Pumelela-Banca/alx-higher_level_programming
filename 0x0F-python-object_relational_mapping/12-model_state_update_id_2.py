18a78a31756a0155af2b30956889d207174c4e9f
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-19 01:54:25 +0200

    updates values

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+adds Louisiana to the db
+"""
+
+import sys
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        """mysql+mysqldb://{}:{}@localhost:3306/{}""".
+        format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id)
+    lookFor = states.filter(states.id == 2).first()
+    lookFor.name = "New Mexico"
+    session.commit()
