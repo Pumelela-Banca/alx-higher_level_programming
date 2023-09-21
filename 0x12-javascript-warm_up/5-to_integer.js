9ab02d3e0e579bd26749d80cb265f787e9df7f9c
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-21 16:51:48 +0200

    converts t ints

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,4 +4,11 @@ const { argv } = require('process');
 
 if (argv[2] === undefined) {
   console.log('Not a number');
-} else if (typeof )
+} else {
+  let num = Number(argv[2]);
+  if (num === NaN) {
+    console.log('Not a number');
+  } else {
+    console.log(parseInt(num, 10));
+  }
+}
