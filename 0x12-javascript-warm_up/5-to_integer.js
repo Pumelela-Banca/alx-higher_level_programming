1fde7f9c9c9618a48f03469f0009439d8124521a
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-21 16:59:02 +0200

    added semmistandard

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,8 +5,8 @@ const { argv } = require('process');
 if (argv[2] === undefined) {
   console.log('Not a number');
 } else {
-  let num = Number(argv[2]);
-  if (num === NaN) {
+  const num = Number(argv[2]);
+  if (isNaN(num)) {
     console.log('Not a number');
   } else {
     console.log('My number: ' + String(parseInt(num, 10)));
