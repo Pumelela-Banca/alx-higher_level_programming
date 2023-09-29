b994d61307929f3e72914d30f6afbce4b985d467
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-30 00:38:08 +0200

    used \t

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,6 +10,6 @@ with request.urlopen(
         'https://alx-intranet.hbtn.io/status') as response:
     content = response.read()
     print("Body response:")
-    print(f"    - type: {type(content)}")
-    print(f"    - content: {content}")
-    print(f"    - utf8 content: {content.decode('utf-8')}")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
+    print(f"\t- utf8 content: {content.decode('utf-8')}")
