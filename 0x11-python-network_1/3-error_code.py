58e2cd24b96a336cf97562f6f449e61b1486f2f5
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-30 01:24:17 +0200

    prints response

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+displays body of url
+"""
+import sys
+from urllib import request
+from urllib import error
+
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as err:
+        print(err)
