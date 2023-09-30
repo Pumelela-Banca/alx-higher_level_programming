3f28ad1842b1389306cab88629eb7c9905c3a316
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-30 02:39:02 +0200

    takes letter and makes a request

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,4 +6,10 @@ Requests header from a web-site
 import sys
 import requests
 
-print(1, "ff")
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        vals = {"q": ""}
+    else:
+        vals = {"q": sys.argv[1]}
+    r = requests.post(sys.argv[1], data=vals)
