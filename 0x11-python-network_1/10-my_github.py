05e44ffa8f086963ae0a6e620f97d4f42bbe9490
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-29 18:12:17 -0700

    llooks for git id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Takes credentials and uses them to show ID
+"""
+
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    inp = HTTPBasicAuth(sys.argv[1], argv[2])
+    r = requests.get("https://api.github.com/user", auth=inp)
+    print(r.json().get('id'))
