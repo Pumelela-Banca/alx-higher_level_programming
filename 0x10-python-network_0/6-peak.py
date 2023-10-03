601f34cdaf12907cad8c912a8fc2b397142f5da9
Pumelela-Banca <pumelelabanca@gmail.com>
2023-10-03 16:57:42 +0200

    added port after IP

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+function finds peak in a list of integers
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    finds peak in a list
+    list_of_integers: list
+    """
+    pass
