e69841bf08301e266f5ffc2c9ffe409a09b38fc3
Pumelela-Banca <pumelelabanca@gmail.com>
2023-10-03 03:53:07 +0200

    posts data 2

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends post and prints results
-curl -s -X POST -d '{"email": "test@gmail.com", "subject":"I will always be here for PLD" }' -X POST "$1"
+curl -sLf "$1" -d "email=test@gmail.com&subject=I will always be here for PLD" -X POST
