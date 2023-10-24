83577979534c78c2682efc1aff769d656ccd3e3b
Pumelela-Banca <pumelelabanca@gmail.com>
2023-10-25 00:54:55 +0200

    prints chracter names

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+const req = require('request');
+
+req(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    for (const charac of JSON.parse(body).characters) {
+      req(charac, (err, res, body) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
