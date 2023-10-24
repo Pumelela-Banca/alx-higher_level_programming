71360986451c9f6a7ed3c148447825e2cf6cd318
Pumelela-Banca <pumelelabanca@gmail.com>
2023-10-25 00:09:36 +0200

    removed trailing white space

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-require('fs').readFile(process.argv[2], 'utf-8', (err, data) => { 
+require('fs').readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
     return console.log(err);
   }
