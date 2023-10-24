d630db306a16b197ac9f805a77a89da83a75cdee
Pumelela-Banca <pumelelabanca@gmail.com>
2023-10-25 01:47:43 +0200

    prints user completed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+require('request')(process.argv[2], (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const done = {};
+    for (const task of JSON.parse(body)) {
+      if (task.completed === true) {
+        if (task.completed[task.userID] === undefined) {
+          done[task.userID] = 0;
+        }
+        done[task.userID] += 1;
+      }
+    }
+    console.log(done);
+  }
+});
