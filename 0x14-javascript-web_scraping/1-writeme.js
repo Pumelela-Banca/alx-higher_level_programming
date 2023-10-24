ee1e9448af02ae86155b8700ac550f4849d1f0dd
Pumelela-Banca <pumelelabanca@gmail.com>
2023-10-25 00:13:24 +0200

    writes a string to file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
 
-const fs = require('fs');
+const fs = ;
 const file = process.argv[2];
+
+require('fs').writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
+  if (err) {
+    return console.error(err);
+  }
+})
