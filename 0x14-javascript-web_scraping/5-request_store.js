d6b6f789940643e050b504bd60ec6924288c7f07
Pumelela-Banca <pumelelabanca@gmail.com>
2023-10-25 01:24:12 +0200

    lists saves into a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+require('request')(process.argv[2], (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    require('fs').writeFile(process.argv[3], body, 'utf-8', (err, data) => {
+      if (err) {
+        return console.log(err);
+      }
+    });
+  }
+});
