bd2f76e8123d454372b35d4dc9da838d56041692
Pumelela-Banca <pumelelabanca@gmail.com>
2023-10-25 00:15:56 +0200

    removed variables

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
 
-const fs = ;
-const file = process.argv[2];
-
 require('fs').writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
   if (err) {
     return console.error(err);
