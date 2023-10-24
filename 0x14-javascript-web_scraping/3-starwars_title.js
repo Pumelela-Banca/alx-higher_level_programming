6141dde6f0e8214c88cc44335e51591c27228c30
Pumelela-Banca <pumelelabanca@gmail.com>
2023-10-25 00:38:00 +0200

    requests from star wars

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+require('request')(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
