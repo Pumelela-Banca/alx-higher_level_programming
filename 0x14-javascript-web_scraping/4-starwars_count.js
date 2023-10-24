00517ddc32fafb2c825ec55bb201375c29440739
Pumelela-Banca <pumelelabanca@gmail.com>
2023-10-25 01:11:23 +0200

    lists all movies that id 18 is in

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+
+const url = process.argv[2];
+
+require('request')(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const data = JSON.parse(body).results;
+    let n = 0;
+    for (const d of data) {
+      const characters = d.characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j].includes('18')) {
+          n++;
+        }
+      }
+    }
+    console.log(n);
+  }
+});
